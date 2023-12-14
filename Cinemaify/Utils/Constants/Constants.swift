96962eca6a48b8149de2e47670991a8d45f32905
enessancar <enes57751907@gmail.com>
2023-12-15 00:22:22 +0300

    Initial Commit

---
+++ Cinemaify/Utils/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Cinemaify
+//
+//  Created by Enes Sancar on 14.12.2023.
+//
+
+import Foundation
+
+struct Constants {
+    
+    enum OnboardPageOne {
+        static let 
+    }
+}
