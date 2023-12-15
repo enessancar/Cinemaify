a95e46a950f20208c850f0c506d7791c85da1d37
enessancar <enes57751907@gmail.com>
2023-12-15 23:56:57 +0300

    data loading vc added

---
+++ Cinemaify/Controllers/DataLoading/MovieDataLoadingVC.swift
@@ -0,0 +1,42 @@
+//
+//  MovieDataLoading.VC.swift
+//  Cinemaify
+//
+//  Created by Enes Sancar on 15.12.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class DataLoadingVC: UIViewController {
+    
+    private var containerView: UIView!
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) {
+            self.containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+                
+        activityIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
+    }
+}
+
