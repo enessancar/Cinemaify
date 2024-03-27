b6169d7a92f2d45975d7313097d2a330c90ff5bc
enessancar <enes57751907@gmail.com>
2024-03-27 10:26:32 +0300

    .

---
+++ Cinemaify/Controllers/Downloads/DownloadsVC.swift
@@ -0,0 +1,15 @@
+//
+//  DownloadsVC.swift
+//  Cinemaify
+//
+//  Created by Enes Sancar on 15.12.2023.
+//
+
+import UIKit
+
+final class DownloadsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
